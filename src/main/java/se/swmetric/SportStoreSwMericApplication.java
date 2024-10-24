56df21310e08363bfceff2b0d12cef7ba48b4c38
techatha <techatham_p@cmu.ac.th>
2024-10-24 14:41:03 +0700

    init

---
+++ src/main/java/se/swmetric/SportStoreSwMericApplication.java
@@ -0,0 +1,13 @@
+package se.swmetric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportStoreSwMericApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportStoreSwMericApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/swmetric/SportStoreSwMericApplicationTests.java
@@ -0,0 +1,13 @@
+package se.swmetric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportStoreSwMericApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
